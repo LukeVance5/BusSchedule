1276626390642c22b53079ce0775a5c2be53f5df
piratepete5 <96152045+piratepete5@users.noreply.github.com>
2022-12-31 13:56:40 -0800

    added bus.jpg, added a button that on click will swap a test paragraph with the next 99 time at sasamat, and made it so it will display now when bus is arriving in less than a minute

--- scripts/translink.js
+++ scripts/translink.js
@@ -2,16 +2,17 @@ async function next99() {
   const response = await fetch("https://api.translink.ca/rttiapi/v1/stops/58023/estimates?apikey=JvXiknT52GzqD8PpJnRp&routeNo=099", {method: "GET",headers: {
     'accept': 'application/json',},});
   const text = await response.text();
-  console.log(text);
   const obj = JSON.parse(text)[0];
-  console.log(obj.Schedules[0].ExpectedCountdown);
-  const main = document.createElement("main");
-  main.innerHTML= `<p>
-  The next 99 will arrive in ${obj.Schedules[0].ExpectedCountdown} minutes
+  let time = obj.Schedules[0].ExpectedCountdown;
+  if (time <=0) {
+    document.getElementById("test").innerHTML=  `<p>
+  The next 99 will arrive now
   </p>`;
-  document.body.appendChild(main);
+  } else {
+    document.getElementById("test").innerHTML=  `<p>
+  The next 99 will arrive in ${time} minutes
+  </p>`;
+  }
   return;
 }
-const main = document.createElement("main");
-main.innerHTML = next99();
 
