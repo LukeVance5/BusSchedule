bdc4138772d89d757dd2931a99faa0c8c9356865
piratepete5 <96152045+piratepete5@users.noreply.github.com>
2023-01-04 13:24:44 -0800

    Added a response to the input, now takes in a bus stop and outputs a unordered list of buses that stop there

--- scripts/translink.js
+++ scripts/translink.js
@@ -1,8 +1,30 @@
-async function next99() {
-  const response = await fetch("https://api.translink.ca/rttiapi/v1/stops/58023/estimates?apikey=JvXiknT52GzqD8PpJnRp&routeNo=099", {method: "GET",headers: {
+let busStop = 00000;
+let RouteNo= 000;
+async function busList(stop) {
+  busStop = stop.trim();
+  let test =document.getElementById("test");
+  try {
+    if (isNaN(parseInt(busStop)) || busStop.length != 5) {
+      throw "Stop must be a 5 digit number, example: 55612"
+    }
+  } catch(err) {
+    test.innerHTML= err;
+  }
+  const response = await fetch("https://api.translink.ca/rttiapi/v1/stops/"+busStop+"?apikey=JvXiknT52GzqD8PpJnRp", {method: "GET",headers: {
     'accept': 'application/json',},});
-  const text = await response.text();
-  const obj = JSON.parse(text)[0];
+  const text = await response.text(); 
+  const obj = JSON.parse(text);
+  const routes = obj.Routes.split(", ");
+  let ul= document.createElement("ul");
+  ul.id= "Bus List";
+  for (let route of routes) {
+    let li = document.createElement("li");
+    li.innerText = route;
+    ul.appendChild(li);
+  } 
+  let action = document.getElementById("action");
+  action.appendChild(ul);
+  /*
   let time = obj.Schedules[0].ExpectedCountdown;
   if (time <=0) {
     document.getElementById("test").innerHTML=  `<p>
@@ -13,6 +35,6 @@ async function next99() {
   The next 99 will arrive in ${time} minutes
   </p>`;
   }
+  */
   return;
 }
-
