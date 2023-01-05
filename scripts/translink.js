554152cdb0a4eddb82a6f9785e0fd75e3e77a93f
piratepete5 <96152045+piratepete5@users.noreply.github.com>
2023-01-04 21:18:39 -0800

    Changed the bus outputs to a dropdown, and when a bus is selected add to dropdown header and remove from items, also added a bit more css

--- scripts/translink.js
+++ scripts/translink.js
@@ -7,34 +7,84 @@ async function busList(stop) {
     if (isNaN(parseInt(busStop)) || busStop.length != 5) {
       throw "Stop must be a 5 digit number, example: 55612"
     }
-  } catch(err) {
-    test.innerHTML= err;
-  }
-  const response = await fetch("https://api.translink.ca/rttiapi/v1/stops/"+busStop+"?apikey=JvXiknT52GzqD8PpJnRp", {method: "GET",headers: {
+    const response = await fetch("https://api.translink.ca/rttiapi/v1/stops/"+busStop+"?apikey=JvXiknT52GzqD8PpJnRp", {method: "GET",headers: {
     'accept': 'application/json',},});
-  const text = await response.text(); 
-  const obj = JSON.parse(text);
-  const routes = obj.Routes.split(", ");
-  let ul= document.createElement("ul");
-  ul.id= "Bus List";
+    const text = await response.text(); 
+    const obj = JSON.parse(text);
+    const routes = obj.Routes.split(", ");
+    createBusList(routes);
+  } catch(err) {
+    let error = document.createElement("p1");
+    error.innerHTML= err;
+    error.id= "error";
+    let output = document.getElementById("output");
+    output.appendChild(error);
+  }
+  
+  return;
+}
+
+function createBusList(routes) {
+  let previousDropDown = document.getElementById("busSelect");
+  let error = document.getElementById("error");
+  if (previousDropDown) {
+    previousDropDown.remove();
+  }
+  if (error) {
+    error.remove();
+  }
+  let busSelect = document.createElement("div");
+  busSelect.className = "dropdown"; 
+  busSelect.id= "busSelect";
+  let select = document.createElement("div");
+  select.className = "select";
+  let selected = document.createElement("span");
+  selected.className = "selected";
+  selected.innerHTML="Select Bus";
+  selected.id= "selectedBus"
+  select.appendChild(selected);
+  busSelect.appendChild(select);
+  let buses= document.createElement("ul");
+  buses.id= "busList";
+  buses.className= "selection";
   for (let route of routes) {
     let li = document.createElement("li");
-    li.innerText = route;
-    ul.appendChild(li);
+    let bus = document.createElement("div");
+    bus.innerText = route;
+    bus.id = route;
+    bus.setAttribute("onclick", "selectBus(this)");
+    bus.value= route;
+    li.appendChild(bus);
+    buses.appendChild(li);
   } 
+  busSelect.appendChild(buses);
   let action = document.getElementById("action");
-  action.appendChild(ul);
-  /*
-  let time = obj.Schedules[0].ExpectedCountdown;
-  if (time <=0) {
-    document.getElementById("test").innerHTML=  `<p>
-  The next 99 will arrive now
-  </p>`;
+  action.appendChild(busSelect);
+  return;
+}
+
+function selectBus(bus) {
+  console.log(bus.value);
+  selectedRoute = bus.value;
+  let selected = document.getElementById("selectedBus");
+  let routes = document.getElementById("busList"); 
+  if (selected.innerHTML=="Select Bus") {
+    selected.innerHTML= selectedRoute;
+    document.getElementById(selectedRoute).parentNode.remove();
   } else {
-    document.getElementById("test").innerHTML=  `<p>
-  The next 99 will arrive in ${time} minutes
-  </p>`;
+    console.log(selected.innerHTML);
+    let oldroute = selected.innerHTML;
+    selected.innerHTML= selectedRoute;
+    document.getElementById(selectedRoute).parentNode.remove();
+    let buses = document.getElementById("busList");
+    let li = document.createElement("li");
+    let bus = document.createElement("div");
+    bus.innerText = oldroute;
+    bus.id = oldroute;
+    bus.setAttribute("onclick", "selectBus(this)");
+    bus.value= oldroute;
+    li.appendChild(bus);
+    buses.appendChild(li);
   }
-  */
   return;
 }
